7f8bd5f2106f834d7439b3daafce9440847aa209
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2020-04-21 17:30:48 -0600

    JavaScript

---
+++ aula/exercicio/exemplo-08-aula11.js
@@ -0,0 +1,9 @@
+var vel = 40.6
+
+console.log(`A velocidade do seu carro é ${vel} km/h`);//COMANDO PARA ESCREVE NO PROMPT DE OUTPUT DO NODE.JS
+
+if(vel<50){
+    console.log('Dirija sempre usando cinto de segurança');
+}else{
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
